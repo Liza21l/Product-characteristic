a7200a8401c9c4b6efb0133936ffbc963ba40c30
Liza21l <Liza2009koshenya@gmail.com>
2023-07-16 15:20:19 +0300

    Product characteristic

---
+++ form/form.js
@@ -0,0 +1,31 @@
+const form = { 
+    name: document.querySelector('input[form-name]'),
+    email: document.querySelector('input[form-email]'),
+    reviews: document.querySelector('input[form-reviews]'),
+    btnClean: document.querySelector('button[form-btn]')
+}
+
+let dataForm 
+if (localStorage.getItem('form')){
+    dataForm = JSON.parse(localStorage.getItem('form'))
+    if (dataForm.name) { 
+        form.name.value = dataForm.name
+    }
+} else { 
+    dataForm = {}
+}
+
+form.name.addEventListener('input', () => {
+   dataForm = { 
+    ...dataForm,
+    name: form.name.value
+   }
+   localStorage.setItem('form', JSON.stringify(dataForm))
+})
+
+form.btnClean.addEventListener('click', () => {
+    localStorage.removeItem('form')
+    location.reload()
+})
+
+
